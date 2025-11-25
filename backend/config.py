d2feb5313594d79c0795c40eae5046d2f2ab0ea0
Mehnaz Shaikh <24f1002437@ds.study.iitm.ac.in>
2025-11-25 23:41:23 +0530

    Add environment variable configuration in config.py

---
+++ backend/config.py
@@ -0,0 +1,5 @@
+import os
+SECRET_STRING = os.environ.get("SECRET_STRING")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+SYSTEM_PROMPT = os.environ.get("SYSTEM_PROMPT")
+USER_PROMPT = os.environ.get("USER_PROMPT")
